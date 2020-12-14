88bd103f1f8689d36527eeddb4f2546e8b953806
mohdh34m <68151199+mohdh34m@users.noreply.github.com>
2020-12-14 21:41:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var color = ["#FE2E2E", "#FF8000", "#FFFF00", "#80FF00", "#00FFFF", "#FF00FF"]
+
+var i = 0;
+document.getElementById("button").addEventListener("click", 
+    function(){
+        i = i < color.length ? ++i : 0;
+        document.querySelector("body").style.background = color[i];
+    })
+
